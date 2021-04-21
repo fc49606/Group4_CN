918a1c0eeb9b7e09e3876a4b9f8f2915a992b848
Daniel Ferrão <80220832+fc49606@users.noreply.github.com>
2021-04-21 20:05:58 +0100

    Update delete_services.sh

--- delete_services.sh
+++ delete_services.sh
@@ -2,8 +2,27 @@ kubectl delete service movie
 kubectl delete service budget 
 kubectl delete service revenue 
 kubectl delete service rating 
+kubectl delete ing ingress -n default
+
+cd microservice_budget
+kubectl delete -f budget-with-proxy.yaml
+cd ..
+
+cd microservice_movies
+kubectl delete -f movie-with-proxy.yaml
+cd ..
+
+cd microservice_rating
+kubectl delete -f rating-with-proxy.yaml
+cd ..
+
+cd microservice_revenue
+kubectl delete -f revenue-with-proxy.yaml
+cd ..
+
 gcloud container clusters delete gk-cluster
+
 gcloud container images delete eu.gcr.io/cn2021-kubernetes-colab/microservice-movies --force-delete-tags --quiet
 gcloud container images delete eu.gcr.io/cn2021-kubernetes-colab/microservice-budget --force-delete-tags --quiet
 gcloud container images delete eu.gcr.io/cn2021-kubernetes-colab/microservice-revenue --force-delete-tags --quiet
-gcloud container images delete eu.gcr.io/cn2021-kubernetes-colab/microservice-ratings --force-delete-tags --quiet
+gcloud container images delete eu.gcr.io/cn2021-kubernetes-colab/microservice-rating --force-delete-tags --quiet

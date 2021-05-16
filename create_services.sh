d34ceb340a173ca49ceb9758864047851d6bed43
Jaime Mendes <jaimerochamendes@gmail.com>
2021-05-16 04:30:19 +0100

    hidden db credentials on budget

--- create_services.sh
+++ create_services.sh
@@ -4,6 +4,7 @@ gcloud container clusters create gk-cluster --num-nodes 3 --machine-type n1-stan
 
 gcloud iam service-accounts keys create key.json --iam-account cn-projeto@cn2021-kubernetes-colab.iam.gserviceaccount.com
 kubectl create secret generic sa-secret --from-file=service_account.json=key.json
+kubectl create secret generic secret-db --from-literal=host=127.0.0.1:5432 --from-literal=username=postgres --from-literal=password=root --from-literal=database=movies --from-literal=datababasestreaming=movies_streaming_plataform
 
 cd microservice_budget
 gcloud builds submit --tag eu.gcr.io/cn2021-kubernetes-colab/microservice-budget
--- microservice_budget/server.py
+++ microservice_budget/server.py
@@ -13,8 +13,27 @@ db = None
 
 def tcp_connection_engine():
 
-    pool = sqlalchemy.create_engine('postgresql+pg8000://postgres:root@127.0.0.1:5432/movies') 
+    # pool = sqlalchemy.create_engine('postgresql+pg8000://postgres:root@127.0.0.1:5432/movies') 
 
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_host = os.environ["DB_HOST"]
+
+    host_args = db_host.split(":")
+    db_hostname, db_port = host_args[0], int(host_args[1])
+
+    pool = sqlalchemy.create_engine(
+            sqlalchemy.engine.url.URL(
+                drivername="postgresql+pg8000",
+                username=db_user,  # e.g. "my-database-user"
+                password=db_pass,  # e.g. "my-database-password"
+                host=db_hostname,  # e.g. "127.0.0.1"
+                port=db_port,  # e.g. 5432
+                database=db_name  # e.g. "my-database-name"
+            )
+    )
+    
     return pool
 
 def default(obj):
